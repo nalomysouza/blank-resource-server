3ef05c3ac79d63cb6fe6dbf25ee62052977b69b3
Nalomy de Souza A. Oliveira <nalomy.souza@secult.ce.gov.br>
2018-03-11 11:50:57 -0300

    projeto usando jsf

--- src/main/java/br/gov/ce/secult/controller/RedirectToIndexController.java
+++ src/main/java/br/gov/ce/secult/controller/RedirectToIndexController.java
@@ -4,12 +4,13 @@ import java.security.Principal;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.ce.secult.config.UserInfo;
 
 @RestController
-public class IndexController {
+public class RedirectToIndexController {
 
 	@RequestMapping("/api")
 	public String test() {
@@ -20,5 +21,10 @@ public class IndexController {
 	@RequestMapping("/principal")
 	public Principal resource(Principal principal) {
 		return principal;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String redirect() {
+		return "redirect:layout.xhtml";
 	}
 }
